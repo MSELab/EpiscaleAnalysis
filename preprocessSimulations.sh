63202d93eb8138258a3e6b84dd688ce7cb4c3f89
Pavel Brodskiy <pavelbrodskiy@gmail.com>
2017-04-16 19:44:33 -0400

    fixed job submission script

--- preprocessSimulations.sh
+++ preprocessSimulations.sh
@@ -1,11 +1,11 @@
 #!/bin/csh
 #$ -N ProcessEpiScale
 #$ -q long
-#$ -pe smp 2
+#$ -pe smp 12
 #$ -M pbrodski@nd.edu
 #$ -m bea
 
-cd /afs/crc.nd.edu/group/Zartman/Pavel/Project3Episcale/Analysis
+setenv MATLABPATH /afs/crc.nd.edu/group/Zartman/Pavel/Project3Episcale/Analysis/
+cd /afs/crc.nd.edu/group/Zartman/Pavel/Project3Episcale/Analysis/
 module load matlab/9.2
-MATLABPATH="/afs/crc.nd.edu/group/Zartman/Pavel/Project3Episcale/Analysis"
-matlab -nodisplay -nosplash -nojvm -r < "analyzeT1Transitions(2)"
+matlab -nodisplay -nosplash -r "analyzeT1Transitions(12,'/afs/crc.nd.edu/group/Zartman/Pavel/Project3Episcale/Analysis')"
