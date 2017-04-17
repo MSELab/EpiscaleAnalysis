69f4b9c29d32442184915e1c73265c3c14332f27
pavelbrodskiy <pavelbrodskiy@gmail.com>
2017-04-17 13:11:15 -0400

    Finished preliminary analysis

---
+++ scratchCellNumber.m
@@ -0,0 +1,30 @@
+close all
+clearvars
+
+load('Summary.mat')
+
+cellNumber = nan(length(Data), 10000);
+
+for i = 1:length(Data)
+    tmp = Data{i}.cellNumber;
+    cellNumber(i,1:length(tmp)) = tmp;
+end
+
+cellNumber = cellNumber(:,any(~isnan(cellNumber),1));
+plot(cellNumber')
+
+frames = sum(~isnan(cellNumber),2);
+N = nanmax(cellNumber,[],2);
+
+minTime = sum(cellNumber<300,2);
+
+minTime((metadata.g_ave==.8))
+
+%%
+C = [1.5,3;2,40;100,450;0,10;2,20;200,450;0,21]'
+nrows = 25
+rlist = candexch(C,nrows)
+
+nfactors = 7
+nruns = 25
+[dCE,X] = cordexch(nfactors,nruns,'linear','bounds',C)
--- scratchT1TransitionFigures.m
+++ scratchT1TransitionFigures.m
@@ -1,7 +1,7 @@
 close all
 clearvars
 
-load('Summary.mat')
+load('SummaryT1.mat')
 
 T1Frequency = [measurements.T1_transition_rate];
 
@@ -20,9 +20,14 @@ T1stdNoMR(isnan(T1aveNoMR)) = 0;
 
 figure(1)
 clf
-errorbar(g_list, T1ave, T1std)
+g_regression = linspace(min(g_ave),max(g_ave),1000);
+errorbar(g_list, T1ave, T1std, '.r')
 hold on
-errorbar(g_list, T1aveNoMR, T1stdNoMR)
+errorbar(g_list, T1aveNoMR, T1stdNoMR, '.k')
+mdl = fitlm(g_ave(MR), T1Frequency(MR), 'quadratic');
+plot(g_regression, mdl.feval(g_regression), '-r');
+mdlNoMR = fitlm([g_ave(~MR)', 0.1,0.1,0.2,0.2,0.3,0.3,0.4,0.4,0.8,0.8]', [T1Frequency(~MR) zeros(1,10)], 'quadratic');
+plot(g_regression, mdlNoMR.feval(g_regression),'-k');
 legend({'Mitotic Rounding', 'No Mitotic Rounding'})
 xlabel('Growth Rate (divisions per hour)')
 ylabel('T1 transitions per hour per 60 cells')
